23ae1f51bcfa8b0b2e723657590c3bc3225bbcc4
janemee <18774042692@163.com>
2023-02-28 18:08:58 +0800

    code format

--- admin/src/main/java/com/huimi/admin/controller/mqtt/MqttGetwayController.java
+++ admin/src/main/java/com/huimi/admin/controller/mqtt/MqttGetwayController.java
@@ -1,23 +1,14 @@
 package com.huimi.admin.controller.mqtt;
 
-import com.huimi.admin.config.shiro.UsernamePasswordCaptchaToken;
 import com.huimi.admin.controller.BaseController;
-import com.huimi.admin.exception.CaptchaException;
-import com.huimi.admin.exception.ForbiddenException;
-import com.huimi.admin.exception.UnAuthorityException;
 import lombok.extern.log4j.Log4j;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.ExcessiveAttemptsException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
